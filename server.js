2127c6fb074ca0dd9018cf0f4ba24fcbc011bfd1
thaonguyen <thaonpgch17640@fpt.edu.vn>
2019-05-03 17:14:55 +0700

    new

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-app.use((req, res, next) => {
-    res.setHeader("X-Frame-Options", "ALLOWALL");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "POST, GET, PUT, DELETE, OPTIONS"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Authorization, Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+const cors = require('cors')
+app.use(cors({
+  origin: ["http://localhost:3000", "https://atntoys41099.herokuapp.com/"],
+  credentials: true
+}));
+// app.use((req, res, next) => {
+//     res.setHeader("X-Frame-Options", "ALLOWALL");
+//     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "POST, GET, PUT, DELETE, OPTIONS"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+//   });
 mongoose.connect("mongodb+srv://Annalanh:Annalanh99@@cluster0-ebnpt.mongodb.net/test?retryWrites=true", {useNewUrlParser: true}, (err) => {
     if(err) console.log(err);
     else console.log("Connected to DB")
